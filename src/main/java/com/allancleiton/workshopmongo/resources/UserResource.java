459d980d33bd32fcbd5657e8934642d694eab73d
Allan Cleiton <allancleitonppma@gmail.com>
2024-04-02 19:31:53 -0300

    Obtendo um usuario por id

--- src/main/java/com/allancleiton/workshopmongo/resources/UserResource.java
+++ src/main/java/com/allancleiton/workshopmongo/resources/UserResource.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +28,11 @@ public class UserResource {
 				.stream()
 				.map(x -> new UserDTO(x))
 				.collect(Collectors.toList()));
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findByID(@PathVariable String id) {
+		return ResponseEntity.ok().body(new UserDTO(userService.findByID(id)));	
 	}
 }
  
--- src/main/java/com/allancleiton/workshopmongo/services/UserService.java
+++ src/main/java/com/allancleiton/workshopmongo/services/UserService.java
@@ -1,10 +1,12 @@
 package com.allancleiton.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.allancleiton.workshopmongo.repositories.UserRepository;
+import com.allancleiton.workshopmongo.services.exceptions.ObjectNotFoundException;
 import com.allancleiton.workshopmongo.domain.User;
 	
 
@@ -15,5 +17,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return userRepository.findAll();
+	}
+	
+	public User findByID(String id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
